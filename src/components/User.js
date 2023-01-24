66e360f3f2b57d484c6928de9efaadb358dd312b
Serge Byusa <sergebyusa@Serges-MacBook-Pro.local>
2023-01-23 18:05:59 -0800

    Added componentDidUpdate, componentDidMount, componentWillUnmount

--- src/components/User.js
+++ src/components/User.js
@@ -4,7 +4,7 @@ import { Component } from 'react';
 class User extends Component {
 
   componentWillUnmount () {
-    console.log('User will unmount');
+    console.log('User will unmount'); // unmounted 3 times cause we have 3 elementsin user
   }
 
   render(){ // specific method that is rendered by react (equivalent to return in functional)
