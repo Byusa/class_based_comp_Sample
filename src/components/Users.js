7b6f055d32f562303cb0b12055759c26881e7549
Serge Byusa <sergebyusa@Serges-MacBook-Pro.local>
2023-01-23 16:23:34 -0800

    added constructor with super() and added initial state and setState and accessing state"

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,52 @@
-import { useState } from 'react';
+import { Component } from 'react';
+import User from './User';
+
+import classes from './Users.module.css';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
+class Users extends Component {
+  constructor() {
+    super()
+    this.state  = {
+      showUsers: true,
+      // moreState: 'Test'
+    }; // in class_base components state is always an object
+  }
+
+  toggleUsersHandler () {
+    this.setState((curState)=> {
+        return { showUsers: !curState.showUsers };
+    })
+    // setShowUsers((curState) => !curState);
+  };
+
+
+  render(){
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+export default Users;
+
+/*import { useState } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -34,4 +82,4 @@ const Users = () => {
   );
 };
 
-export default Users;
+export default Users;*/
